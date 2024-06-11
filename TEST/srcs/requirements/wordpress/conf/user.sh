c631b6146c8ca36b99101b7c4ba1837b53fb6a68
Alexisflore <alexisflorentin14@gmail.com>
2024-06-11 16:55:52 +0200

    fsd

--- TEST/srcs/requirements/wordpress/conf/user.sh
+++ TEST/srcs/requirements/wordpress/conf/user.sh
@@ -1,14 +1,15 @@
+
 wp core install --url=localhost --title="Inception" --admin_user=${DB_USER} --admin_password=${DB_PASS} --admin_email=${DB_EMAIL} --skip-email
-if wp user get user42 >/dev/null 2>&1; then
-    echo "User user42 exists."
-else
-   wp user create ${USER} ${WP_EMAIL} --role=subscriber --user_pass=${DB_PASS} --skip-email
-fi
+echo "hahaah"
+
+wp user create ${USER} ${WP_EMAIL} --role=subscriber --user_pass=${DB_PASS} --skip-email
+
 wp user update "${USER}" --user_pass="${DB_PASS}" --skip-email
+
 if wp user get "${USER}" >/dev/null 2>&1; then
     echo "Logged in successfully as root."
 else
     echo "Login failed. Please check your root credentials."
 fi
 wp theme activate twentytwentytwo
-/usr/sbin/php-fpm83 -F
+# /usr/sbin/php-fpm83 -F
